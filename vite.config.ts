f53494e818e81bb6dd3f4aff1776c8221f0a646b
Adrian Hartanto <adrian.hartanto@traveloka.com>
2023-08-03 17:06:49 +0700

    add RNW

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    define: {
+      __DEV__: mode === "development",
+    },
+    resolve: {
+      alias: {
+        "react-native": "react-native-web",
+      },
+    },
+  };
+});
