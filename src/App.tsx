baa5d00a9bfb9cf1deccfa480ca8ba7ff6df0c3a
Adrian Hartanto <adrian.hartanto@traveloka.com>
2023-08-04 14:32:44 +0700

    add district libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { DRNBadge, DRNButton, R } from "@traveloka/district-ui";
+import IcBrandTraveloka from "@traveloka/district-icon/IcBrandTraveloka";
+import DRNViewDescription from "@traveloka/district-view-description";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: R.spacing.xl,
+      }}
+    >
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,22 +24,180 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <Text style={{ fontSize: 24, fontWeight: "bold", color: "#fff" }}>
-        RNW
+      <Text
+        style={{
+          ...R.typography.hero,
+          color: R.color.lightPrimary,
+        }}
+      >
+        Vite + RNW + TS
       </Text>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <IcBrandTraveloka width={60} height={60} style={{ display: "block" }} />
+      <DRNBadge variant="ALERT" label="DRNBadge" />
+      <DRNButton
+        text="DRNButton"
+        onPress={() => alert("DRNButton.onPressed")}
+      />
+      <View style={{ width: "50%", backgroundColor: R.color.lightNeutral }}>
+        <DRNViewDescription
+          data={[
+            {
+              id: "5c6a3f9ad0238",
+              componentName: "CardView",
+              type: "VIEW",
+              children: [
+                {
+                  id: "5c6a3f9ad0399",
+                  componentName: "Title",
+                  type: "VIEW",
+                  children: [],
+                  marginTop: null,
+                  marginBottom: null,
+                  marginLeft: null,
+                  marginRight: null,
+                  paddingTop: null,
+                  paddingBottom: null,
+                  paddingLeft: null,
+                  paddingRight: null,
+                  backgroundColor: null,
+                  width: null,
+                  height: null,
+                  styleSheet: {
+                    marginTop: 16,
+                    marginHorizontal: 24,
+                  },
+                  styleSheetGroup: undefined,
+                  validations: [],
+                  text: "1. Shinjuku Gyoen",
+                  style: "TITLE_3",
+                  color: null,
+                  uid: "IvsevP-fzB",
+                  parentUID: "wMGRVKBBc",
+                },
+                {
+                  id: "5c6a3f9ad0511",
+                  componentName: "ImageGallery",
+                  type: "VIEW",
+                  children: [],
+                  marginTop: null,
+                  marginBottom: null,
+                  marginLeft: null,
+                  marginRight: null,
+                  paddingTop: null,
+                  paddingBottom: null,
+                  paddingLeft: null,
+                  paddingRight: null,
+                  backgroundColor: null,
+                  width: null,
+                  height: 200,
+                  styleSheet: {
+                    marginTop: 16,
+                    marginHorizontal: 16,
+                  },
+                  styleSheetGroup: undefined,
+                  validations: [],
+                  imageUrls: [
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTdkw4AcqfuwrwrLrjC2MKsLVay7KhI4xbjA&usqp=CAUhttps://tvlk-blog.imgix.net/2019/02/Tokyo-Shinjuku-Gyoen-1017018754.jpg?auto=compress&fm=pjpg&ixlib=php-1.2.1",
+                  ],
+                  uid: "43r86xCe7_",
+                  parentUID: "wMGRVKBBc",
+                },
+                {
+                  id: "5c6a3f9ad05c3",
+                  componentName: "Paragraph",
+                  type: "VIEW",
+                  children: [],
+                  marginTop: null,
+                  marginBottom: null,
+                  marginLeft: null,
+                  marginRight: null,
+                  paddingTop: null,
+                  paddingBottom: null,
+                  paddingLeft: null,
+                  paddingRight: null,
+                  backgroundColor: null,
+                  width: null,
+                  height: null,
+                  styleSheet: {
+                    lineHeight: 24,
+                    marginTop: 16,
+                    marginHorizontal: 24,
+                    fontSize: 16,
+                  },
+                  styleSheetGroup: undefined,
+                  validations: [],
+                  htmlText:
+                    "Bosan berkeliling kota ataupun berbelanja di Tokyo? Coba ke Shinjuku Gyoen saja! Sebagai salah satu taman kota tercantik di Tokyo, Shinjuku Gyoen menjadi tempat yang populer untuk menikmati bunga sakura sambil bersantai. Maka dari itu, tak perlu heran jika kamu melihat ada banyak pengunjung yang justru berpiknik di bawah teduhnya pepohonan di area ini. Terbagi ke dalam beberapa taman utama, tempat wisata ini memiliki koleksi berbagai jenis tanaman tropis maupun subtropis.",
+                  style: "BODY_1",
+                  color: null,
+                  uid: "BjJqWDVBOw",
+                  parentUID: "wMGRVKBBc",
+                },
+              ],
+              marginTop: null,
+              marginBottom: null,
+              marginLeft: null,
+              marginRight: null,
+              paddingTop: null,
+              paddingBottom: null,
+              paddingLeft: null,
+              paddingRight: null,
+              backgroundColor: null,
+              width: null,
+              height: null,
+              styleSheet: {
+                marginBottom: 8,
+              },
+              styleSheetGroup: undefined,
+              validations: [],
+              uid: "wMGRVKBBc",
+            },
+            {
+              id: "RBPa6c8z4",
+              componentName: "Countdown",
+              type: "VIEW",
+              endTimestamp: 1674324614000,
+              columnTitle: {
+                day: "HARI",
+                hour: "JAM",
+                minute: "MENIT",
+                second: "DETIK",
+              },
+              color: {
+                number: "#232323",
+                border: "blue",
+                text: "white",
+                numberBackground: "white",
+                textBackground: "blue",
+                colon: "blue",
+              },
+              uid: "RBPa6c8z4",
+            },
+            {
+              id: "lDxQg581B",
+              componentName: "ProductWidget",
+              type: "VIEW",
+              title: "enim unde velit",
+              subTitle: "Strackestad",
+              imageUrl:
+                "https://ideastest.org.uk/wp-content/uploads/2019/06/default-avatar-150x150.jpg",
+              ratingLabel: "Rating: ",
+              rating: "7.8/10",
+              price: "Rp 640.000",
+              deepLink: "traveloka://home",
+              styleSheetGroup: {
+                container: {},
+                image: {},
+                subTitleText: {},
+                titleText: {},
+                priceText: {},
+              },
+              uid: "lDxQg581B",
+            },
+          ]}
+        />
+      </View>
+    </div>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// order define priority
+const extensions = [".web.tsx", ".web.ts", ".web.js", ".tsx", ".ts", ".js"];
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
@@ -9,8 +12,20 @@ export default defineConfig(({ mode }) => {
       __DEV__: mode === "development",
     },
     resolve: {
+      extensions,
       alias: {
         "react-native": "react-native-web",
+        "react-native-linear-gradient": "react-native-web-linear-gradient",
+        "react-native-svg": "svgs",
+      },
+    },
+    optimizeDeps: {
+      force: true,
+      esbuildOptions: {
+        resolveExtensions: extensions,
+        loader: {
+          ".js": "jsx",
+        },
       },
     },
   };
